ec4e5dc0545261cbf213b30c8bb98382831b07fd
RayFFH <57190209+RayFFH@users.noreply.github.com>
2024-03-17 19:58:19 +0000

    Adding map to index.html

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
 const express = require('express');
-const { request } = require('follow-redirects').https;
 const fs = require('fs');
-const auth_key = Buffer.from('access_key:secret_key').toString('base64');
-
-const options = {
-  method: 'GET',
-  hostname: 'api.roadgoat.com',
-  port: 80,
-  path: '/api/v2/destinations/new-york-ny-usa',
-  headers: {
-    Authorization: `Basic ${auth_key}`
-  },
-  maxRedirects: 20
-};
+require('dotenv').config();
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, 'env', '.env') });
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  const reqApi = request(options, (response) => {
-    let chunks = '';
+const HTML_FILE_PATH = path.join('public', 'index.html');
 
-    response.on('data', (chunk) => {
-      chunks += chunk;
+// Read the API key from environment variables
+app.use(express.static(path.join(__dirname, 'public')));
+const apiKey = process.env.API_KEY;
+
+if (!apiKey) {
+    console.error('API key not found in environment variables.');
+    return;
+}
+
+// Read the HTML file
+fs.readFile(HTML_FILE_PATH, 'utf8', (err, htmlContent) => {
+    if (err) {
+        console.error('Error reading HTML file:', err);
+        return;
+    }
+
+    // Replace placeholder with the API key in the HTML content
+    const updatedHtmlContent = htmlContent.replace('{{API_KEY}}', apiKey.trim());
+
+    // Write the updated HTML content back to the file
+    fs.writeFile(HTML_FILE_PATH, updatedHtmlContent, 'utf8', err => {
+        if (err) {
+            console.error('Error writing updated HTML file:', err);
+            return;
+        }
+        console.log('API key injected into HTML file successfully.');
     });
-
-    response.on('end', () => {
-      const responseBody = JSON.parse(chunks);
-      const photos = responseBody.included.filter(
-        (item) => item.type === 'photo'
-      );
-      // Extracting photo URLs
-      const photoUrls = photos.map((photo) => {
-        return photo.attributes.image.medium; // or 'thumb' for thumbnail
-      });
-      res.json(photoUrls);
-    });
-  });
-
-  reqApi.on('error', (error) => {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  });
-
-  reqApi.end();
 });
 
 app.listen(port, () => {
